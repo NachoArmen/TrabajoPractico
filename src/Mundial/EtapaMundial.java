c079faf250bcaddcbfc7527810fd238fb5bbb060
escuela@davinci.edu.ar <ignacio.armendariz@davinci.edu.ar>
2022-11-17 10:33:06 -0300

    feat: create EtapaMundial and classes

---
+++ src/Mundial/EtapaMundial.java
@@ -0,0 +1,17 @@
+package Mundial;
+import java.util.ArrayList;
+
+
+public class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList<Partido> partidos;
+
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void setDescripcionEtapa(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+    }
+}
