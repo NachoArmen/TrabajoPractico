6b6128ef029c73bfbd94b4f797a9e02e834dc8e6
escuela@davinci.edu.ar <ignacio.armendariz@davinci.edu.ar>
2022-11-28 10:14:04 -0300

    feat: created constructors

--- src/Mundial/EtapaMundial.java
+++ src/Mundial/EtapaMundial.java
@@ -7,11 +7,25 @@ public class EtapaMundial {
     private ArrayList<Partido> partidos;
 
 
+
+    EtapaMundial(){
+
+    }
     public String getDescripcionEtapa() {
+
         return descripcionEtapa;
     }
 
     public void setDescripcionEtapa(String descripcionEtapa) {
+
         this.descripcionEtapa = descripcionEtapa;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
     }
 }
--- src/Mundial/Partido.java
+++ src/Mundial/Partido.java
@@ -9,8 +9,17 @@ public class Partido {
   private Resultado resultado;
 
 
-    public Resultado getResultado() {
-        return resultado;
+    Partido(Date fecha, Equipo local, Equipo visitante){
+        this.fecha= fecha;
+        this.local= local;
+        this.visitante= visitante;
+    }
+
+
+
+  public Resultado getResultado() {
+
+      return resultado;
     }
 
     public Equipo getLocal() {
