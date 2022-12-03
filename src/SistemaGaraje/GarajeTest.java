1ce669e6b4cd623d420d4a3eb50fed65b178a831
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2022-12-03 10:04:36 -0300

    feat: get & set for garaje

--- src/SistemaGaraje/GarajeTest.java
+++ src/SistemaGaraje/GarajeTest.java
@@ -3,4 +3,11 @@ package SistemaGaraje;
 public class GarajeTest {
     private Garaje garaje;
 
+    public Garaje getGaraje() {
+        return garaje;
+    }
+
+    public void setGaraje(Garaje garaje) {
+        this.garaje = garaje;
+    }
 }
