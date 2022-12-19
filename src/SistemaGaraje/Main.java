e8f54d9a504f3e81bb697f658db44dad58ba37c6
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2022-12-19 19:33:58 -0300

    feat: implementation on Main(code finished)

--- src/SistemaGaraje/Main.java
+++ src/SistemaGaraje/Main.java
@@ -1,6 +1,7 @@
 package SistemaGaraje;
 import Mundial.Equipo;
 import Mundial.Grupo;
+import Mundial.Llave;
 import Vehiculos.*;
 
 import java.util.ArrayList;
@@ -9,12 +10,12 @@ public class Main {
     public static void main(String[] args) {
 
          // GARAJE
-         Garaje garajePrincipal= new Garaje(8,4);
-         Coche vwGol= new Coche("volkswagen",300,"LLP271","gris",3);
-         Coche audiA5= new Coche("audi",150,"AD179CB","negro",5);
-         Coche bmwX8= new Coche("bmw", 2000, "IUF834", "blanco", 5);
-         Moto suzukiR9= new Moto("100cc", "Suzuki", "ABC123","verde", 2500,2);
-         Moto kawasakiNinja= new Moto("500cc", "Kawasaki", "XYZ789","rojo", 6000,2);
+         Garaje garajePrincipal = new Garaje(8, 4);
+         Coche vwGol = new Coche("volkswagen", 300, "LLP271", "gris", 3);
+         Coche audiA5 = new Coche("audi", 150, "AD179CB", "negro", 5);
+         Coche bmwX8 = new Coche("bmw", 2000, "IUF834", "blanco", 5);
+         Moto suzukiR9 = new Moto("100cc", "Suzuki", "ABC123", "verde", 2500, 2);
+         Moto kawasakiNinja = new Moto("500cc", "Kawasaki", "XYZ789", "rojo", 6000, 2);
 
 
          garajePrincipal.addVehiculo(audiA5);
@@ -22,10 +23,10 @@ public class Main {
          garajePrincipal.addVehiculo(vwGol);
          garajePrincipal.addVehiculo(kawasakiNinja);
 
-         System.out.println(String.format("¿Esta completo?: %b \n Capacidad actual: %s \n Promedio de Kilometraje: %f \n Precio total de las ruedas: %f", garajePrincipal.topeMaximo(),garajePrincipal.getVehiculos().size(),garajePrincipal.promedioKilometraje(),garajePrincipal.precioTotalRuedas()));
+         System.out.println(String.format("¿Esta completo?: %b \n Capacidad actual: %s \n Promedio de Kilometraje: %f \n Precio total de las ruedas: %f", garajePrincipal.topeMaximo(), garajePrincipal.getVehiculos().size(), garajePrincipal.promedioKilometraje(), garajePrincipal.precioTotalRuedas()));
 
 
-          // MUNDIAL
+         // MUNDIAL
 
          // CREACION DE "GRUPOS"
          ArrayList<Equipo> grupo1 = new ArrayList<Equipo>();
@@ -37,7 +38,7 @@ public class Main {
          ArrayList<Equipo> grupo7 = new ArrayList<Equipo>();
          ArrayList<Equipo> grupo8 = new ArrayList<Equipo>();
 
-        // "SORTEO" DE EQUIPOS
+         // "SORTEO" DE EQUIPOS
 
          //GRUPO 1
          grupo1.add(new Equipo("A"));
@@ -89,25 +90,133 @@ public class Main {
 
 
          // PARTIDOS A JUGAR
-         Grupo partidosGrupo1= new Grupo();
-         Grupo partidosGrupo2= new Grupo();
-         Grupo partidosGrupo3= new Grupo();
-         Grupo partidosGrupo4= new Grupo();
-         Grupo partidosGrupo5= new Grupo();
-         Grupo partidosGrupo6= new Grupo();
-         Grupo partidosGrupo7= new Grupo();
-         Grupo partidosGrupo8= new Grupo();
+         Grupo partidosGrupo1 = new Grupo();
+         Grupo partidosGrupo2 = new Grupo();
+         Grupo partidosGrupo3 = new Grupo();
+         Grupo partidosGrupo4 = new Grupo();
+         Grupo partidosGrupo5 = new Grupo();
+         Grupo partidosGrupo6 = new Grupo();
+         Grupo partidosGrupo7 = new Grupo();
+         Grupo partidosGrupo8 = new Grupo();
 
 
+         partidosGrupo1.jugarPartidos(grupo1);
+         partidosGrupo1.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo1.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo1.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo1.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo1.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo1.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo2.jugarPartidos(grupo2);
+         partidosGrupo2.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo2.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo2.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo2.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo2.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo2.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo3.jugarPartidos(grupo3);
+         partidosGrupo3.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo3.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo3.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo3.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo3.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo3.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo4.jugarPartidos(grupo4);
+         partidosGrupo4.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo4.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo4.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo4.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo4.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo4.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo5.jugarPartidos(grupo5);
+         partidosGrupo5.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo5.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo5.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo5.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo5.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo5.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo6.jugarPartidos(grupo6);
+         partidosGrupo6.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo6.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo6.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo6.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo6.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo6.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo7.jugarPartidos(grupo7);
+         partidosGrupo7.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo7.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo7.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo7.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo7.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo7.getPartidos().get(5).anotarGoles(1, 7);
+
+         partidosGrupo8.jugarPartidos(grupo8);
+         partidosGrupo8.getPartidos().get(0).anotarGoles(3, 3);
+         partidosGrupo8.getPartidos().get(1).anotarGoles(0, 2);
+         partidosGrupo8.getPartidos().get(2).anotarGoles(4, 3);
+         partidosGrupo8.getPartidos().get(3).anotarGoles(2, 1);
+         partidosGrupo8.getPartidos().get(4).anotarGoles(5, 2);
+         partidosGrupo8.getPartidos().get(5).anotarGoles(1, 7);
 
 
+         // PASAN EQUIPOS CLASIFICADOS
 
+         ArrayList<Equipo> clasOctavos = new ArrayList<Equipo>();
+         clasOctavos.addAll(partidosGrupo1.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo2.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo3.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo4.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo5.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo6.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo7.getEquiposAvanzan());
+         clasOctavos.addAll(partidosGrupo8.getEquiposAvanzan());
 
+         // JUEGAN OCTAVOS DE FINAL
 
+         Llave octavosDeFinal = new Llave();
+         octavosDeFinal.jugarPartidos(clasOctavos);
+         octavosDeFinal.getPartidos().get(0).anotarGoles(3, 0);
+         octavosDeFinal.getPartidos().get(1).anotarGoles(3, 2);
+         octavosDeFinal.getPartidos().get(2).anotarGoles(2, 3);
+         octavosDeFinal.getPartidos().get(3).anotarGoles(0, 3);
+         octavosDeFinal.getPartidos().get(4).anotarGoles(6, 1);
+         octavosDeFinal.getPartidos().get(5).anotarGoles(4, 0);
 
+         // JUEGAN CUARTOS DE FINAL
 
+         ArrayList<Equipo> clasCuartos = octavosDeFinal.getEquiposAvanzan();
+         Llave cuartosDeFinal = new Llave();
+         cuartosDeFinal.jugarPartidos(clasCuartos);
+         cuartosDeFinal.getPartidos().get(0).anotarGoles(1, 0);
+         cuartosDeFinal.getPartidos().get(1).anotarGoles(0, 2);
+         cuartosDeFinal.getPartidos().get(2).anotarGoles(1, 2);
+         cuartosDeFinal.getPartidos().get(3).anotarGoles(5, 0);
 
+         // JUEGAN SEMIFINALES
 
+         ArrayList<Equipo> clasSemis = cuartosDeFinal.getEquiposAvanzan();
+         Llave semiFinal= new Llave();
+         semiFinal.jugarPartidos(clasSemis);
+         semiFinal.getPartidos().get(0).anotarGoles(3,0);
+         semiFinal.getPartidos().get(1).anotarGoles(2,1);
+
+         // JUEGAN LA FINAL
+         ArrayList<Equipo> clasFinal= semiFinal.getEquiposAvanzan();
+         Llave finalDelMundial= new Llave();
+         finalDelMundial.jugarPartidos(clasFinal);
+         finalDelMundial.getPartidos().get(0).anotarGoles(4,2);
+
+         // GANADOR DE LA MEJOR COPA
+
+         ArrayList<Equipo> ganadorMundial= finalDelMundial.getEquiposAvanzan();
+
+         System.out.println(String.format("Glorioso Campeon del Mundo: %s",ganadorMundial.get(0).getNombreEquipo()));
 
 
 
