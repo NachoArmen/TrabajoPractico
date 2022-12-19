fc0457b4d0f0a672e0f608ddf9732f8c775f9e11
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2022-12-19 12:56:15 -0300

    feat: implementation on  LLave

--- src/Mundial/Llave.java
+++ src/Mundial/Llave.java
@@ -1,4 +1,24 @@
 package Mundial;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Llave extends EtapaMundial {
+
+    public void jugarPartidos(ArrayList<Equipo> equipos){
+        for (int i=0; i<equipos.size(); i+=2){
+            getPartidos().add(new Partido(new Date(),equipos.get(i),equipos.get(i+1));
+        }
+    }
+
+    public ArrayList<Equipo> getEquiposAvanzan(){
+        for (int i=0; i< getPartidos().size();i++){
+            if (getPartidos().get(i).getResultado().isGanoLocal()){
+                this.getEquiposAvanzan().add(getPartidos().get(i).getLocal());
+            }else{
+                this.getEquiposAvanzan().add(getPartidos().get(i).getVisitante());
+            }
+        }
+        return getEquiposAvanzan();
+    }
 }
