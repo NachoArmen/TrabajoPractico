f0f6bb002ea1d0839bf4f2c1f16d172ae7af87f0
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2022-12-19 12:17:59 -0300

    feat: worked on Equipo

--- src/Mundial/Equipo.java
+++ src/Mundial/Equipo.java
@@ -2,15 +2,59 @@ package Mundial;
 import java.util.ArrayList;
 
 public class Equipo {
-    private String nombre;
+    private String nombreEquipo;
+    private int puntos;
+    private int puntajeTotal;
     private ArrayList<Partido> partidos;
 
 
-    public String getNombre() {
-        return nombre;
+
+
+    Equipo(){
+
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
     }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getPuntajeTotal() {
+        return puntajeTotal;
+    }
+
+    public void setPuntajeTotal(int puntajeTotal) {
+        this.puntajeTotal = puntajeTotal;
+    }
+
+
+    // CALCULO DE PUNTAJE
+
+    public void puntajeTotal(){
+        this.setPuntos(3);
+        puntajeTotal=puntos+puntajeTotal;
+        this.setPuntos(puntajeTotal);
+    }
+
+
 }
